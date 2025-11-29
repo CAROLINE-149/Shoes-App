7f8caee39bc75a5f9150d0828b0fbddde1d23d15
devannkanini <kanininjuguna715@gmail.com>
2025-11-30 00:32:17 +0300

    Updated the logo and favicon

--- shoeProject/shoeAttic/views.py
+++ shoeProject/shoeAttic/views.py
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
 from django.shortcuts import render,redirect
 
 from django.http import HttpResponse
 from .forms import shoeForm
 from .models import Shoe
-=======
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import ShoeForm 
->>>>>>> c09a507ea825e346f973d2081cb1d24e8ffa8931
+from .forms import shoeForm 
 # Create your views here.
 def home(request): 
         return render(request, 'shoeAttic/home.html ')
 # Create your views here.
 def createShoe(request):
-<<<<<<< HEAD
     form = shoeForm()
-=======
     form = ShoeForm()
->>>>>>> c09a507ea825e346f973d2081cb1d24e8ffa8931
 
     if request.method == "POST":
         form = shoeForm(request.POST) # gets the data from what the user has input
@@ -26,7 +20,6 @@ def createShoe(request):
             form.save()
             return redirect("readShoe")
     context = {"form": form}
-<<<<<<< HEAD
     return render(request, "shoeAttic/forms.html", context)
 
 def readShoe(request):
@@ -61,6 +54,4 @@ def deleteShoe(request, pk):
         return redirect("readShoe")
     context ={"shoes":shoe}
     return render(request, "shoeAttic/delete.html", context)
-=======
     return render(request, "shoeAttic/forms.html", context)
->>>>>>> c09a507ea825e346f973d2081cb1d24e8ffa8931
