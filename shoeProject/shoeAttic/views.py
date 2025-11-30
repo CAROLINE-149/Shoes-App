564b34b606bc5219404a9fed7f604405b6333634
Mangeni-03 <mangeniallan03@gmail.com>
2025-11-30 14:15:52 +0300

    update

--- shoeProject/shoeAttic/views.py
+++ shoeProject/shoeAttic/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import ShoeForm
+from .forms import shoeForm
 from .models import Shoe
 
 # Home view
